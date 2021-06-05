85331009ed3ddbc1df2af08adcbd5bc26aed2e3e
Lokesh Ravichandru <Lokesh.ravichandru@grootan.com>
2021-06-05 11:58:50 +0530

    1.0.0 version push

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# unomi-studio
-A simplified analytics dashboard and api for Apache unomi
+# Unomi Studio
+Dashboard to manage apps and tenants for tracking and analytics
+
+## How to run
+
+## Docker
+```
+docker-compose build
+```
+
+```
+docker-compose up -d
+```
+
+Wait till unomi docker container is up and running
+
+```
+cd scripts
+./post-deploy-script.sh
+```
+
+## Dev mode
+
+Remove unomi-node from docker-compose and repeat above steps
+
+Set following environment variables.
+
+```
+export UNOMI_HOST=http://localhost:8181
+export UNOMI_USERNAME=karaf
+export UNOMI_PASSWORD=karaf
+export ELASTIC_HOST=http://localhost:9200
+```
+Go into api folder and run following commands
+
+```
+npm install
+```
+
+```
+nodemon index.js
+```
+or 
+```
+node index.js
+```
+
+## UI
+
+Run following url in browser
+
+```
+http://localhost:8085
+```
+
+### Tenant View
+
+![tenants](./img/tenants.png)
+
+### Apps View
+
+![apps](./img/apps.png)
+
+### Dashboard View
+
+![dashboard](./img/dashboard.png)
+
+## Rest Api
+
+Unomi studio exposes api and ui
+
+
+```
+Base Path: http://localhost:8085/v1/api
+```
+
+# Documentation
+
+Read the [docs](https://unomi-studio.netlify.app)
+
+# Issues
+
+For issues please report [here](https://github.com/grootan/unomi-studio/issues)
+
+# Roadmap
+
+-> Addition and deletion of Rules
+-> Add Support for goals
+-> Add Support for campaigns
---
+++ scripts/post-deploy-script.sh
@@ -0,0 +1,86 @@
+curl --location --request POST 'http://localhost:8181/cxs/rules' \
+--header 'Authorization: Basic a2FyYWY6a2FyYWY=' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "itemId": "_ajhg9u2s5_sessionAssigned",
+    "itemType": "rule",
+    "version": 1,
+    "condition": {
+        "parameterValues": {
+            "subConditions": [
+                {
+                    "parameterValues": {
+                        "eventTypeId": "sessionCreated"
+                    },
+                    "type": "eventTypeCondition"
+                },
+                {
+                    "parameterValues": {
+                        "eventTypeId": "sessionReassigned"
+                    },
+                    "type": "eventTypeCondition"
+                }
+            ],
+            "operator": "or"
+        },
+        "type": "booleanCondition"
+    },
+    "actions": [
+        {
+            "parameterValues": {
+                "setPropertyName": "properties.previousVisit",
+                "setPropertyValue": "profileProperty::lastVisit",
+                "storeInSession": false
+            },
+            "type": "setPropertyAction"
+        },
+        {
+            "parameterValues": {
+                "setPropertyName": "properties.lastVisit",
+                "setPropertyValue": "now",
+                "storeInSession": false
+            },
+            "type": "setPropertyAction"
+        },
+        {
+            "parameterValues": {
+                "setPropertyName": "properties.nbOfVisits",
+                "setPropertyValue": "script::profile.properties.?nbOfVisits != null ? (profile.properties.nbOfVisits + 1) : 1",
+                "storeInSession": false
+            },
+            "type": "setPropertyAction"
+        },
+        {
+            "parameterValues": {
+                "setPropertyName": "systemProperties.scope",
+                "setPropertyValue": "eventProperty::scope",
+                "storeInSession": false
+            },
+            "type": "setPropertyAction"
+        }
+    ],
+    "linkedItems": null,
+    "raiseEventOnlyOnceForProfile": false,
+    "raiseEventOnlyOnceForSession": false,
+    "raiseEventOnlyOnce": false,
+    "priority": 0,
+    "metadata": {
+        "id": "_ajhg9u2s5_sessionAssigned",
+        "name": "Session assigned to a profile",
+        "description": "Update profile visit information",
+        "scope": "systemscope",
+        "tags": [],
+        "systemTags": [],
+        "enabled": true,
+        "missingPlugins": false,
+        "hidden": false,
+        "readOnly": true
+    }
+}'
+
+
+docker cp ../misc/allCountries.zip unomi:/opt/apache-unomi/etc
+
+docker cp ../misc/GeoLite2-City.mmdb unomi:/opt/apache-unomi/etc
+
+docker restart unomi
